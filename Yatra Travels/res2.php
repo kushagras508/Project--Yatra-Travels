46253e4e9c51f13e0ea9df7ea16c0e1da2f7d3ab
kushagras508 <90048289+kushagras508@users.noreply.github.com>
2022-07-21 00:50:31 +0530

    Update res2.php

--- Yatra Travels/res2.php
+++ Yatra Travels/res2.php
@@ -1,9 +1,10 @@
 <?php
 
-$db_user = "root";
-$db_pass = "";
-$db_name = "host";
+$db_user = "epiz_32203801";
+$db_pass = "7IIIAp7cFsap";
+$db_name = "epiz_32203801_host";
 
-$db = new PDO('mysql:host=localhost;dbname='. $db_name. ';charset=utf8', $db_user ,$db_pass);
+
+$db = new PDO('mysql:host=sql302.epizy.com;dbname='. $db_name. ';charset=utf8', $db_user ,$db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 ?>
